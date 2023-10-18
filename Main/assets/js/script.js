06574ca0b0868884ea5e437428f6f9fd45ce0785
baseduli <ulisesvivar123@gmail.com>
2023-10-18 14:16:53 -0400

    able to fetch the data correctly now through the function getWeather. API key working and allowing the data to pull through. Created functions for displayForecast and displayWeather which work to create cards that display the data. Possibly need to hardcode some of the CSS to target the new cards created.

--- Main/assets/js/script.js
+++ Main/assets/js/script.js
@@ -1,4 +1,5 @@
 var inputFormEl = document.querySelector(".form-input");
+var APIKey = "a8fe8e270b30883ded902415fd9d7831";
 
 function handleFormSubmit(event) {
     event.preventDefault();
@@ -13,15 +14,40 @@ function handleFormSubmit(event) {
         alert("Please enter a city name");
     }
 }
+// need to include displayWeather funciton and displayForecast function here to create the cards for the weather and forecast
+function displayForecast(data) {
+    var cityCard = document.querySelector('#city-forecast');
+    var temp = data.main.temp;
+    var card = document.createElement('div');
+    card.innerHTML = `
+    <p>Temperature: ${temp}</p>
+    <p>Wind: ${data.wind.speed} MPH</p>
+    <p>Humidity: ${data.main.humidity} %</p>
+    `;
+    cityCard.appendChild(card);
+}
+
+function displayWeather(data) {
+    var weatherCard = document.querySelector('#five-day');
+    var temp = data.main.temp;
+    var card = document.createElement('div');
+    card.innerHTML = `
+    <p>Temperature: ${temp}</p>
+    <p>Wind: ${data.wind.speed} MPH</p>
+    <p>Humidity: ${data.main.humidity} %</p>
+    `;
+    weatherCard.appendChild(card);
+}
 
 function getWeather(city) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=3f0f9a8e4f2b1c8e0c4e6f9b9b4a5e6d";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=a8fe8e270b30883ded902415fd9d7831";
 
-    fetch(apiUrl)
+    fetch(queryURL)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    displayWeather(data, city);
+                    console.log(data);
+                    displayWeather(data);
                     displayForecast(data, city);
                 });
             } else {
