14317cda899c1a7caa79345ade3336c06e021ee0
Ulises <ulisesvivar123@gmail.com>
2023-10-20 20:10:19 -0400

    worked with tutor, realized there is a separate API for the 5 day forecast. Worked on creating a for loop to targer the array of hours inside the 40 arrays representing 5 days in 3 hour increments. Code still needs styling

--- Main/assets/js/script.js
+++ Main/assets/js/script.js
@@ -1,5 +1,9 @@
 var inputFormEl = document.querySelector(".form-input");
 var APIKey = "a8fe8e270b30883ded902415fd9d7831";
+var temp;
+var wind;
+var humidity;
+
 
 function handleFormSubmit(event) {
     event.preventDefault();
@@ -15,29 +19,6 @@ function handleFormSubmit(event) {
     }
 }
 
-function displayForecast(data) {
-    var cityCard = document.querySelector('#city-forecast');
-    var temp = data.main.temp;
-    var card = document.createElement('card');
-    card.innerHTML = `
-    <p>Temperature: ${temp}</p>
-    <p>Wind: ${data.wind.speed} MPH</p>
-    <p>Humidity: ${data.main.humidity} %</p>
-    `;
-    cityCard.appendChild(card);
-}
-
-function displayWeather(data) {
-    var weatherCard = document.querySelector('#five-day');
-    var temp = data.main.temp;
-    var card = document.createElement('card');
-    card.innerHTML = `
-    <p>Temperature: ${temp}</p>
-    <p>Wind: ${data.wind.speed} MPH</p>
-    <p>Humidity: ${data.main.humidity} %</p>
-    `;
-    weatherCard.appendChild(card);
-}
 
 function getWeather(city) {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=a8fe8e270b30883ded902415fd9d7831";
@@ -46,9 +27,11 @@ function getWeather(city) {
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    console.log(data);
+                    temp = data.main.temp;
+                    wind = data.wind.speed;
+                    humidity = data.main.humidity;
                     displayWeather(data);
-                    displayForecast(data, city);
+                    getForecast(data.coord.lat, data.coord.lon);
                 });
             } else {
                 alert("Error: " + response.statusText);
@@ -57,4 +40,59 @@ function getWeather(city) {
         .catch(function (error) {
             alert("Unable to connect to OpenWeather");
         });
+}
+
+
+function getForecast(lat, lon) {
+    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=a8fe8e270b30883ded902415fd9d7831`;
+
+    fetch(queryURL)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    // console.log(`fiveDays -> ${JSON.stringify(data)}`);
+                    displayForecast(data);
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert("Unable to connect to OpenWeather");
+        });
+}
+
+function displayForecast(data) {
+    console.log(data)
+    let count = 8;
+    let forecastData = [0, count, count * 2, count * 3, count * 4];
+
+    for (var i = 0; i < forecastData.length; i++) {
+        console.log(forecastData[i]);
+        temp = data.list[forecastData[i]].main.temp;
+        humidity = data.list[forecastData[i]].main.humidity;
+        wind = data.list[forecastData[i]].wind.speed;
+        var cityCard = document.querySelector('#city-forecast');
+        var card = document.createElement('div');
+        // div.classList.add('forecast-box');
+        card.innerHTML = `
+        <p>Temperature: ${temp}</p>
+        <p>Wind: ${wind} MPH</p>
+        <p>Humidity: ${humidity} %</p>
+        `;
+        cityCard.appendChild(card);
+    }
+
+}
+
+function displayWeather(data) {
+    var weatherCard = document.querySelector('#five-day');
+    var card = document.createElement('div');
+    // div.classList.add('forecast-card');
+    card.innerHTML = `
+    <p>Temperature: ${temp}</p>
+    <p>Wind: ${wind} MPH</p>
+    <p>Humidity: ${humidity} %</p>
+    `;
+    weatherCard.appendChild(card);
 }
