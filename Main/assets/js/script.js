2a19707e8cccc0322d8674f1be759adcb6530983
Ulises <ulisesvivar123@gmail.com>
2023-11-01 19:24:01 -0400

    added buttons for what is stored locally. The weather cards and box update according to the city search. adding screen shots and working on readme

--- Main/assets/js/script.js
+++ Main/assets/js/script.js
@@ -4,16 +4,24 @@ var temp;
 var wind;
 var humidity;
 var time;
-
+var cityName;
+var previousSearches = JSON.parse(localStorage.getItem("previousSearches")) || [];
+var weatherIcon;
 
 function handleFormSubmit(event) {
     event.preventDefault();
 
     var searchInputVal = document.querySelector("#search-input").value;
     var searchInputValTrimmed = searchInputVal.trim();
-    localStorage.setItem(searchInputValTrimmed, searchInputValTrimmed);
 
     if (searchInputValTrimmed) {
+
+        if (!previousSearches.includes(searchInputValTrimmed)) {
+            previousSearches.push(searchInputValTrimmed);
+            localStorage.setItem("previousSearches", JSON.stringify(previousSearches));
+            createSearchButton(searchInputValTrimmed);
+        }
+
         getWeather(searchInputValTrimmed);
         inputFormEl.reset();
     } else {
@@ -21,7 +29,21 @@ function handleFormSubmit(event) {
     }
 }
 
+function createSearchButton(city) {
+    var button = document.createElement("button");
+    button.textContent = city;
+    button.classList.add("btn");
+    button.addEventListener("click", function () {
+        getWeather(city);
+    });
+    document.getElementById("previousSearches").appendChild(button);
+}
 
+function loadPreviousSearches() {
+    previousSearches.forEach(function (city) {
+        createSearchButton(city);
+    });
+}
 
 function getWeather(city) {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=a8fe8e270b30883ded902415fd9d7831";
@@ -33,6 +55,8 @@ function getWeather(city) {
                     temp = data.main.temp;
                     wind = data.wind.speed;
                     humidity = data.main.humidity;
+                    cityName = data.name;
+                    weatherIcon = data.weather[0].icon;
                     //time = parseInt(data.dt);
                     console.log(data);
                     displayWeather(data);
@@ -71,6 +95,8 @@ function displayForecast(data) {
     console.log(data)
     let count = 8;
     let forecastData = [0, count, count * 2, count * 3, count * 4];
+    var cityCard = document.querySelector('#five-day');
+    cityCard.innerHTML = "";
 
     for (var i = 0; i < forecastData.length; i++) {
         console.log(forecastData[i]);
@@ -81,7 +107,8 @@ function displayForecast(data) {
         var card = document.createElement('div');
         card.classList.add('forecast-card');
         card.innerHTML = `
-        <p>Temperature: ${temp}</p>
+        <h2>${weatherIcon}</h2>
+        <p>Temp: ${temp}°F</p>
         <p>Wind: ${wind} MPH</p>
         <p>Humidity: ${humidity} %</p>
         `;
@@ -92,11 +119,13 @@ function displayForecast(data) {
 
 function displayWeather(data) {
     var weatherCard = document.querySelector('#city-forecast');
+    weatherCard.innerHTML = "";
+
     var card = document.createElement('div');
     card.classList.add('forecast-box');
     card.innerHTML = `
-    <h2>${name}</h2>
-    <p>Temperature: ${temp}</p>
+    <h2>${cityName} ${weatherIcon}</h2>
+    <p>Temp: ${temp}°F </p>
     <p>Wind: ${wind} MPH</p>
     <p>Humidity: ${humidity} %</p>
     `;
